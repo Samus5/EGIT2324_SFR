e81105ad9d552af467ba9f5cdefa5ef6805408b5
Samus5 <a_samuel.fernandez.rodriguez@iespablopicasso.es>
2023-11-29 09:55:48 +0100

    Subida del programa persona

---
+++ EXGIT2324/src/Persona.java
@@ -0,0 +1,59 @@
+
+public class Persona {
+	    // Propiedades
+	    private String nombre;
+	    private String apellidos;
+	    private int edad;
+
+	    // Constructor
+	    public Persona(String nombre, String apellidos, int edad) {
+	        this.nombre = nombre;
+	        this.apellidos = apellidos;
+	        this.edad = edad;
+	    }
+
+	    // Método para sumar 1 a la edad e imprimir por pantalla
+	    public void cumpleaños() {
+	        edad++; // Sumar 1 a la edad
+	        System.out.println("¡Feliz cumpleaños! Ahora tienes " + edad + " años.");
+	    }
+
+	    // Métodos para obtener los valores de las propiedades
+	    public String getNombre() {
+	        return nombre;
+	    }
+
+	    public String getApellidos() {
+	        return apellidos;
+	    }
+
+	    public int getEdad() {
+	        return edad;
+	    }
+
+	    // Métodos para establecer los valores de las propiedades
+	    public void setNombre(String nombre) {
+	        this.nombre = nombre;
+	    }
+
+	    public void setApellidos(String apellidos) {
+	        this.apellidos = apellidos;
+	    }
+
+	    public void setEdad(int edad) {
+	        this.edad = edad;
+	    }
+
+	    public static void main(String[] args) {
+	        // Ejemplo de uso
+	        Persona persona = new Persona("Juan", "Pérez", 25);
+	        System.out.println("Nombre: " + persona.getNombre());
+	        System.out.println("Apellidos: " + persona.getApellidos());
+	        System.out.println("Edad: " + persona.getEdad());
+
+	        // Llamada al método cumpleaños
+	        persona.cumpleaños();
+	    }
+	}
+
+
